6f282041dab314d989d3e70cb83328e52f97fc09
BlueFeather <bluefeatherwashere@gmail.com>
2019-08-22 09:19:53 +0200

    ps util

--- util_win.ps1
+++ util_win.ps1
@@ -4,59 +4,54 @@ Function hook {
     [string]$W
   )
   Process {
+    $i = -1
     $dest = "E:\code\hook\pkg"
+    $dest2 = "E:\code\aard\worlds\plugins"
+    $fileName = "Hook_MiniEpics", "Hook_Shanty", "Hook_Spellup"
+    $fldrFrm = "E:\code\hook\$($fileName[0])", "E:\code\hook\$($fileName[1])", "E:\code\hook\$($fileName[2])"
     switch ($W) {
-      miniepics {
-        "MiniEpics: starting the packing procedure..."
-        $what = "E:\code\hook\Hook_MiniEpics\Hook_MiniEpics*"
-        $zipfile = "$dest\miniepics.zip"
-        if (Test-Path $zipfile) {rm $zipfile}
-        Compress-Archive -Path $what -CompressionLevel Optimal -DestinationPath $zipfile
-        "MiniEpics: package complete."
-      }
-      shanty {
-        "Shanty: starting the packing procedure..."
-        $what = "E:\code\hook\Hook_Shanty\Hook_Shanty*"
-        $zipfile = "$dest\shanty.zip"
-        if (Test-Path $zipfile) {rm $zipfile}
-        Compress-Archive -Path $what -CompressionLevel Optimal -DestinationPath $zipfile
-        "Shanty: package complete."
-      }
-      spellup {
-        "Spellup: starting the packing procedure..."
-        $what = "E:\code\hook\Hook_Spellup\Hook_Spellup*"
-        $zipfile = "$dest\spellup.zip"
-        if (Test-Path $zipfile) {rm $zipfile}
-        Compress-Archive -Path $what -CompressionLevel Optimal -DestinationPath $zipfile
-        "Spellup: package complete."
-      }
-      default {
-        "Options are: miniepics, shanty, spellup."
-      }
+      miniepics { $i = 0 }
+      shanty { $i = 1 }
+      spellup { $i = 2 }
+      default { $i = -1 }
+    }
+    if ($i -gt -1) {
+      Write-Host "$($fileName[$i].Remove(0,5)): copying files to aard folder..." -NoNewline
+      $destFile = "$($dest2)\$($fileName[$i]).xml"
+      $srcFile = "$($fldrFrm[$i])\$($fileName[$i]).xml"
+      if (Test-Path $destFile) { Remove-Item $destFile }
+      Copy-Item $srcFile -Destination $destFile
+      Write-Host " done."
+
+      $what = "E:\code\hook\$($fileName[$i])\$($fileName[$i])*"
+      $zipfile = "$($dest)\$($fileName[$i].Remove(0,5).ToLower()).zip"
+      Write-Host "$($fileName[$i].Remove(0,5)): packing $($fileName[$i].Remove(0,5).ToLower()).zip for distribution..." -NoNewline
+      if (Test-Path $zipfile) { Remove-Item $zipfile }
+      Compress-Archive -Path $what -CompressionLevel Optimal -DestinationPath $zipfile
+      Write-Host " done."
+
+    } else {
+      Write-Host "Options are: miniepics, shanty, spellup."
     }
   }
 }
 
 Function touch {
   $file = $args[0]
-  if($file -eq $null) {
+  if ($null -eq $file) {
     throw "No filename supplied"
   }
-  if(Test-Path $file) {
+  if (Test-Path $file) {
     (Get-ChildItem $file).LastWriteTime = Get-Date
-  } else {
+  }
+  else {
     New-Item -ItemType File -Path $file
   }
 }
 
 Function sync {
-  if($null -eq $args[0] -Or $null -eq $args[1]) {
-    throw "Usage: sync source destination."
+  if ($null -eq $args[0] -Or $null -eq $args[1]) {
+    throw "Usage: sync source destination"
   }
-  # Remove trailing backslash
-  $src = $args[0] -replace '\\$'
-  $src = '"' + $src + '"'
-  $dest = $args[1] -replace '\\$'
-  $dest = '"' + $dest + '"'
-  Robocopy $src $dest /MIR /FFT /Z /XA:H /W:5
+  Robocopy $args[0] $args[1] /MIR /FFT /Z /XA:H /W:5
 }
